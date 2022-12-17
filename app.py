25ee3db9a6ee201eb940e520aa81918de9ada2ca
Yuang Tong <yuangtong1999@gmail.com>
2022-12-17 12:07:04 -0800

    learning js+html+css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,request,render_template
+from flask import Flask,url_for,request,render_template,redirect
 
 app = Flask(__name__)
 
@@ -11,11 +11,21 @@ def game():
         user2 = request.form["name2"]
         mode = request.form["radio"]
         if mode == "pvb":
-            return render_template("pvbgame.html",username1=user1)
+            return redirect(url_for('pvbgame',username1=user1))
+            # return render_template("pvbgame.html",username1=user1)
         if mode == "pvp":
+            return redirect(url_for('pvpgame',username1=user1,username2=user2))
             return render_template("pvpgame.html",username1=user1,username2=user2)
         
     return render_template("index.html")
+
+@app.route("/pvbgame/<username1>")
+def pvbgame(username1):
+    return render_template("pvbgame.html",username1=username1)
+
+@app.route("/pvpgame/<username1> vs <username2>")
+def pvpgame(username1,username2):
+    return render_template("pvpgame.html",username1=username1,username2=username2)
 
 if __name__ =="__main__":
     app.run(debug=True, port=8080)
---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+function Onclicked(pos){
+    //id = document.
+    //alert(document.getElementById('gamemode').getAttribute("value"));
+    var button = document.getElementById("block_"+pos);
+    alert(button);
+}
